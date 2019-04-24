93cf76b4fd57f6c335ec131145afda697e5778a3
Kian Hooshmand <kianhooshmand@berkeley.edu>
2019-04-24 11:08:04 -0700

    removed old files

--- heuristic.py
+++
@@ -1,8 +0,0 @@
-
-def heuristic(l_resources, vp, flag):
-    resource_weight = 3 #np.array([1, 1, 1])
-    victory_point_weight = 5
-    made_move_weight = 0.3
-
-    #return np.dot(l_resources, resource_weight.T) + vp*victory_point_weight + flag*made_move_weight
-    return resource_weight*l_resources + vp*victory_point_weight + flag*made_move_weight
