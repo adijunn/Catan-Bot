d8451b78d6245e3ceda0be40224e2c60aa57e2c2
Aditya Ganapathi <adivganapathi@airbears2-10-142-108-229.airbears2.1918.berkeley.edu>
2019-04-24 00:36:11 -0700

    changed heuristic.py

--- heuristic.py
+++ heuristic.py
@@ -1,5 +1,7 @@
 
-def heuristic(state):
-	resource_weight = np.array([])
-	victory_point_weight = 
-	made_move_weight = 	
+def heuristic(l_resources, vp, flag):
+	resource_weight = np.array([1, 1, 1])
+	victory_point_weight = 5
+	made_move_weight = 0.3
+
+	return np.dot(l_resources, resource_weight.T) + vp*victory_point_weight + flag*made_move_weight	
