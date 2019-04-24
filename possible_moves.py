cdf48d5f6476d0310c0ba235cf4808a544bfafa0
Kian Hooshmand <kianhooshmand@berkeley.edu>
2019-04-24 00:37:21 -0700

    adee

--- possible_moves.py
+++ possible_moves.py
@@ -7,7 +7,7 @@ def get_possible_settlements(board):
 			if board.if_can_build("settlement", x, y, 0):
 				new_settlement = ('s', (x,y))
 				possible_settlements.append(new_settlement)
-	return possible_settlements
+	return [possible_settlements]
 
 def get_possible_roads(board):
 	possible_roads = []
@@ -17,16 +17,16 @@ def get_possible_roads(board):
 			v2 = board.get_vertex_number(x, y+1)
 			if board.if_can_build(v1, v2, 0):
 				new_road = ('r', (x,y), (x,y+1))
-				possible_roads.append(new_road)	
+				possible_roads.append(new_road)
 	for y in range(5):
 		for x in range(4):
 			v1 = board.get_vertex_number(x, y)
 			v2 = board.get_vertex_number(x+1, y)
 			if board.if_can_build(v1, v2, 0):
 				new_road = ('r', (x,y), (x+1,y))
-				possible_roads.append(new_road)	
-	return possible_roads
-				
+				possible_roads.append(new_road)
+	return [possible_roads]
+
 
 def possible_moves(board, player):
 	moves = []
@@ -35,7 +35,6 @@ def possible_moves(board, player):
 	if player.if_can_buy("road"):
 		moves += get_possible_roads(board)
 	if player.if_can_buy("card"):
-		moves += ('v')
+		moves += [('v')]
 
 	return moves
-	
