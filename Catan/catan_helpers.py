fd532cde2478bc2b0efca360132001c001087e47
Aditya Ganapathi <adivganapathi@airbears2-10-142-108-229.airbears2.1918.berkeley.edu>
2019-04-24 00:27:16 -0700

    added discount rate

--- Catan/catan_helpers.py
+++ Catan/catan_helpers.py
@@ -48,6 +48,7 @@ def calculate_expected_resources(board):
     dice = board.dice
     resources = board.resources
     values = np.zeros((board.height+1, board.width+1))
+    discount_rate = 0.1
 
     probabilities = [0, 0, 1/36, 2/36, 3/36, 4/36, 5/36, 0, 5/36, 4/36, 3/36, 2/36, 1/36]
 
@@ -72,5 +73,6 @@ def calculate_expected_resources(board):
 
             #top right point
             values[x+1][y+1] += r
-
+    
+    values /= (1 - discount_rate)
     return values
--- heuristic.py
+++ heuristic.py
@@ -1,3 +1,5 @@
 
 def heuristic(state):
 	resource_weight = np.array([])
+	victory_point_weight = 
+	made_move_weight = 	
