e60576e887a7a47816e5726e7b469e757be4d86f
Kian Hooshmand <kianhooshmand@berkeley.edu>
2019-04-24 11:48:51 -0700

    fixed planboard for initial placement

--- Catan/Functions/planboard.py
+++ Catan/Functions/planboard.py
@@ -3,13 +3,13 @@ def planBoard(baseBoard):
     big_x = 0
     big_y = 0
     max_val = 0
-    for x in range(baseBoard.width):
-        for y in range(baseBoard.height):
+    for x in range(baseBoard.width+1):
+        for y in range(baseBoard.height+1):
             if vals[x][y] > max_val:
                 big_x = x
                 big_y = y
                 max_val = vals[x][y]
 
-    optSettlementLoc = (x, y)
-
+    optSettlementLoc = (big_y, big_x)
+    
     return optSettlementLoc
